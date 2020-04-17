4f4acddfe675d7270b72c290378b6fde4e7a7c57
pinyana <60167177+pinyana@users.noreply.github.com>
2020-04-16 22:10:39 -0400

    Home Screen Patch

--- main.dart
+++ main.dart
@@ -1,3 +1,4 @@
+import 'categories_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -5,25 +6,12 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Meals', home: MyHomePage(),);
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final String navTime = 'tacoman';
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Meals'),
+    return MaterialApp(
+      title: 'Meals',
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
       ),
-      body: Center(child: Text('$navTime'),),
+      home: CategoriesScreen(),
     );
   }
 }
